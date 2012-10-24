2edfaf1f83a5551326985e990775179c255f51fe
Rahul Kumar <rahulsinner@gmail.com>
2012-10-24 17:21:54 +0530

    Adding http auth.

--- blueprints/post/__init__.py
+++ blueprints/post/__init__.py
@@ -1,5 +1,5 @@
 from lib import utils
 from config import settings
 
-BEFORE_REQUESTS = [utils.http_auth(settings.HTTP_USERNAME, settings.HTTP_PASSWORD,
-                                   'post.new', 'post.delete', 'post.edit', 'post.comment_delete')]
+BEFORE_REQUESTS = [utils.http_dont_auth(settings.HTTP_USERNAME, settings.HTTP_PASSWORD,
+                                        'post.index', 'post.show', 'post.comment_new')]
