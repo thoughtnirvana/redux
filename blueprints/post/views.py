c0946ccfaf3b766018a0bd8dc9f344b70d168a8b
Rahul Kumar <rahulsinner@gmail.com>
2012-12-17 20:03:18 +0530

    Misc.

--- blueprints/post/views.py
+++ blueprints/post/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, request
 from config import db
 import models
 import forms
