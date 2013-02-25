428f47869c330e2f9267b87c1a38ab8403851f4a
Rahul Kumar <rahulsinner@gmail.com>
2013-02-26 03:45:51 +0530

    Changed model_form syntax.

--- blueprints/post/forms.py
+++ blueprints/post/forms.py
@@ -1,17 +1,17 @@
-import flaskext.wtf as wtf
-from flaskext.wtf import Form, validators
+import flask.ext.wtf as wtf
 from wtforms.ext.sqlalchemy.orm import model_form
+from flask.ext.wtf import Form, validators
 import models
 
 
-PostForm = model_form(models.Post, Form, field_args = {
+PostForm = model_form(models.Post, models.db.session, base_class=Form, field_args = {
     'name': {'validators': [validators.required()]},
     'title': {'validators': [validators.required(), validators.length(min=5)]},
     'content': {'validators': []},
     'tags': {'validators': []},
 })
 
-CommentForm = model_form(models.Comment, Form, field_args = {
+CommentForm = model_form(models.Comment, models.db.session, base_class=Form, field_args = {
     'commenter': {'validators': [validators.required()]},
     'body': {'validators': [validators.required()]},
 })
