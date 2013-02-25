2da7a2e378c562efba9b48954182a0d885cc56bb
Rahul Kumar <rahulsinner@gmail.com>
2013-02-26 03:56:22 +0530

    Removing sample blueprint.

--- blueprints/post/__init__.py
+++
@@ -1,5 +0,0 @@
-from lib import utils
-from config import settings
-
-BEFORE_REQUESTS = [utils.http_dont_auth(settings.HTTP_USERNAME, settings.HTTP_PASSWORD,
-                                        'post.index', 'post.show', 'post.comment_new')]
